1ad9f882ebed365c141b002ae581885665ef6f27
millionhari <mikemhari@gmail.com>
2015-04-02 15:06:05 -0700

    (feat) Implement server using Node, Express, and Mongoose

---
+++ server.js
@@ -0,0 +1,12 @@
+// Server-Side Entry Point
+// -----------------------
+//
+// var app = require()
+
+// This is where we set the port equal to the environment's variable when deployed, or to 8000 if on a localhost.
+var port = process.env.PORT || 8000;
+
+// Listen to port
+app.listen(port);
+
+console.log('Server is now listening on port ' + port);
