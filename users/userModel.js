a0dc0f7031683e9d96c1fe8fc758aa0146cfe6df
Stephanie A. Higa <sahiga@gmail.com>
2015-04-03 15:50:24 -0700

    (feat) Edit user model

--- users/userModel.js
+++ users/userModel.js
@@ -6,7 +6,11 @@
 var mongoose = require('mongoose');
 
 var UserSchema = new mongoose.Schema({
-
+  githubID: String,
+  avatar: {
+    type: String,
+    name: String
+  }
 });
 
-module.exports = mongoose.model('users', UserSchema);
+module.exports = mongoose.model('User', UserSchema);
