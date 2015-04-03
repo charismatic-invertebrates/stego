b72015a40b532367a6defea3d1a54a32767fac44
Stephanie A. Higa <sahiga@gmail.com>
2015-04-03 15:50:24 -0700

    (feat) Edit user model

--- users/userController.js
+++ users/userController.js
@@ -1,1 +1,11 @@
+// User Controller
+// ---------------
+//
+// The user controller handles requests passed from the user router.
 
+var Q = require('q');
+var User = require('./userModel.js');
+
+module.exports = {
+
+};
--- users/userRoutes.js
+++ users/userRoutes.js
@@ -4,3 +4,8 @@
 // This file routes requests to /api/users in the middleware to specific User methods defined in the User controller.
 
 var userController = require('./userController.js');
+
+module.exports = function(app) {
+  app.route('/')
+    .get(userController)
+}
