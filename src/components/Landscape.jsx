241ed45d991cfb6ade2e797f50272a3068a8937a
Stephanie A. Higa <sahiga@gmail.com>
2015-04-06 16:00:34 -0700

    (feat) Add Chrome extension setup

---
+++ src/components/Landscape.jsx
@@ -0,0 +1,35 @@
+var $ = require('jquery');
+var React = require('react');
+
+var Landscape = React.createClass({
+
+  getInitialState: function() {
+    var now = new Date().getHours();
+    var timeOfDay;
+
+    // wee hours of the morning and night
+    // between 12:00 a.m. and 6:00 a.m.
+    // or between 6:00 p.m. and 12:00 a.m.
+    if ((now >= 0 && now < 6) || (now >= 18 && now <= 24)) {
+      timeOfDay = 'night';
+
+    // morning for normal humans
+    } else if (now >= 6 && now < 12) {
+      timeOfDay = 'morning';
+
+    // afternoon
+    } else if (now >= 12 && now < 18) {
+      timeOfDay = 'afternoon';
+    }
+
+    return timeOfDay;
+  },
+
+  render: function() {
+    return (
+      <div className="{this.state.timeOfDay}"></div>
+    );
+  }
+});
+
+module.exports = Landscape;
---
+++ src/main/stego.js
@@ -0,0 +1,6 @@
+var $ = require('jquery');
+var React = require('react');
+
+var Landscape = require('./components/Landscape.jsx');
+
+React.render(<Landscape />, document.getElementById('stego'));
