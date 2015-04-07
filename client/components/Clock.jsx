15b0d394d72723e7541439647a7f7772e9a46179
Stephanie A. Higa <sahiga@gmail.com>
2015-04-06 20:13:55 -0700

    (feat) Add meridians and eight landscapes

--- client/components/Clock.jsx
+++ client/components/Clock.jsx
@@ -4,28 +4,51 @@ var Clock = React.createClass({
 
   getInitialState: function() {
     return {
-      time: this.checkTime()
+      time: this.checkTime(),
+      meridian: this.checkMeridian()
     };
   },
 
   checkTime: function() {
     var now = new Date();
-    var hour = now.getHours() - 12;
-    var minutes = now.getMinutes();
+    var hour = now.getHours();
+    var minutes = now.getMinutes().toString();
 
-    return hour + ':' + minutes;
+    if (minutes.length === 1) {
+      minutes = '0' + minutes;
+    }
+
+    return (hour - 12) + ':' + minutes;
+  },
+
+  checkMeridian: function() {
+    var now = new Date();
+    var hour = now.getHours();
+    var meridian;
+
+    if (hour < 12) {
+      meridian = 'a.m.';
+    } else {
+      meridian = 'p.m.'
+    }
+
+    return meridian;    
   },
 
   componentDidMount: function() {
     setInterval(function() {
       this.setState({time: this.checkTime()});
+      this.setState({meridian: this.checkMeridian()});
     }.bind(this), 60000);
   },
 
   render: function() {
     return (
       <div className="clock">
-        <h1>{this.state.time}</h1>
+        <h1>
+          {this.state.time}
+          <span className="meridian">{this.state.meridian}</span>
+        </h1>
       </div>
     );
   }
