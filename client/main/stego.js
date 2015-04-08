3ba0604299f544d0651d2b1fdcc4d6fe42e6d779
millionhari <mikemhari@gmail.com>
2015-04-08 01:26:52 -0700

    (refactor) Update grunt file to minify css, and build jsx files
    - Restructured application to use dist and src folders

--- client/main/stego.js
+++
@@ -1,9 +0,0 @@
-var $ = require('jquery');
-var React = require('react');
-
-var App = require('./App.jsx');
-var Landscape = require('../components/Landscape.jsx');
-
-$(document.body).append('<div id="app"></div>');
-
-React.render(<App />, document.getElementById('app'));
