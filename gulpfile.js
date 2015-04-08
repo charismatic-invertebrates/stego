498dd8ac32921705e929ec08391b5eaeb09410c8
millionhari <mikemhari@gmail.com>
2015-04-08 10:58:38 -0700

    (refactor) Edit gulp file to copy HTML file and images to public

--- gulpfile.js
+++ gulpfile.js
@@ -8,12 +8,15 @@ var notify = require('gulp-notify');
 var minifyCSS = require('gulp-minify-css');
 
 var path = {
-  HTML: './client/main/stego.html',
+  HTML_SRC: './client/src/stego.html',
+  HTML_PUBLIC: './client/dist/public',
   ENTRY_POINT: './client/src/js/App.jsx',
   OUT: 'bundle.js',
   PUBLIC: './client/dist/public/js',
   CSS_SRC: './client/src/css/style.css',
-  CSS_PUBLIC: './client/dist/public/css'
+  CSS_PUBLIC: './client/dist/public/css',
+  IMAGES_SRC: './client/src/images/**',
+  IMAGES_PUBLIC: './client/dist/public/images'
 };
 
 gulp.task('build', function(){
@@ -25,6 +28,10 @@ gulp.task('build', function(){
   .pipe(source(path.OUT))
   .pipe(gulp.dest(path.PUBLIC))
   .pipe(notify('Stego Build Complete!'));
+  gulp.src([path.HTML_SRC])
+  .pipe(gulp.dest(path.HTML_PUBLIC));
+  gulp.src([path.IMAGES_SRC])
+  .pipe(gulp.dest(path.IMAGES_PUBLIC));
 });
 
 gulp.task('styles', function(){
@@ -33,3 +40,16 @@ gulp.task('styles', function(){
   .pipe(gulp.dest(path.CSS_PUBLIC))
   .pipe(notify('Styles Build Complete!'));
 });
+
+// gulp.task('watch', function(){
+//   var watcher = watchify(build({
+//     entries: [path.ENTRY_POINT],
+//     transform: [reactify],
+//     debug: true,
+//     cache: {}, packageCache: {}, fullPaths: true
+//   }));
+
+//   return watcher.on('update', function(){
+
+//   })
+// })
