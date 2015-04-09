bb0f886cdb369e0502dc9fb6aa353030c5651fb4
ChadFong <ChadMFong@Gmail.com>
2015-04-09 14:41:07 -0700

    (add) save 'this' context to auth property for use during AJAX
    In order to change the state of our user properties we need to be
      able to affect this.state and setState(), but we lose the 'this'
      context necessary to do so in AJAX.  To maintain this context we
      faux IIFE auth.

--- client/src/js/App.jsx
+++ client/src/js/App.jsx
@@ -11,69 +11,117 @@ var App = React.createClass({
     // Here we should check if there is already an existing user, and set the userInfo values as such.
 
     return {
+
+      // This property holds all user properties
+      userInfo: {
+        github: {
+          name: null,
+          username: null,
+          reposUrl: null,
+          token: null
+        },
+        fitness: {
+          username: null,
+        }
+      }
     };
   },
+  
+  // This is a faux-IIFE for auth so that auth can save the 'this' context.  A regular IIFE statement does not render the correct context.
+  componentWillMount: function(){
+    this.auth();
+  },
 
-  auth: {
-    userInfo: {
-      github: {
-        requestGithub: function(apiRequest) {
-          var self = this;
-          $.ajax({
-            type: 'GET',
-            url: 'https://api.github.com' + apiRequest,
-            data: {access_token: self.token},
-            success: function(res){
-              console.log(res);
-            }
-          });
-        },
-        token: null,
-      }
-    },
+  // This property holds all Authentication logic.
+  auth: function(){
+    var context = this;
+    console.log(context, "does it save correctly");
+    console.log('what is this in the iife', this);
 
-    login: function(provider) {
-      var self = this;
-      var url = '';
-      console.log(provider);
-      switch(provider) {
-        case 'github':
-          url = 'https://github.com/login/oauth/authorize?client_id=' + keys.github.clientID;
-          break;
-        case 'fitbit':
-          url = 'https://www.fitbit.com/oauth/request_token?oauth_consumer_key=' + keys.fitbit.consumerKey;
-          break;
-        case 'jawbone':
-          url = 'https://jawbone.com/auth/oauth2/auth?response_type=code&client_id=' + keys.jawbone.clientID;
-          break;
-      }
-      console.log(url);
-      chrome.identity.launchWebAuthFlow(
-        // This portion works with all APIs
-        {'url': url, 'interactive': true},
-        function(redirectUrl) {
-          var code = redirectUrl.split('?code=')[1];
-          // This AJAX call is coded to work with github
-          $.ajax({
-            type: 'POST',
-            url: 'https://github.com/login/oauth/access_token',
-            data: {client_id: keys.github.clientID,
-                   client_secret: keys.github.clientSecret,
-                   code: code
-                  },
-            redirect_uri: 'https://bmlpebnpaikchpcabnbieodibjbhcggf.chromiumapp.org/githubToken',
-            success: function(res) {
-              var token = res.match(/(?:access_token=)[a-zA-Z0-9]+/g)[0].split('access_token=')[1];
-              console.log(self);
-              self.userInfo.github.token = token;
-            },
-            fail: function(err) {
-              console.error(err);
+    // userInfo: {
+    //   github: {
+    //     requestGithub: function(apiRequest, callback) {
+    //       var self = this;
+    //       $.ajax({
+    //         type: 'GET',
+    //         url: 'https://api.github.com' + apiRequest,
+    //         data: {access_token: self.token},
+    //         success: function(res){
+    //           console.log(res);
+    //           callback(res);
+    //         }
+    //       });
+    //     },
+    //     username: null,
+    //     alias: null,
+    //     reposUrl: null,
+    //     token: null,
+    //   }
+    // },
+    return {
+      login: function(provider) {
+        var self = this;
+        console.log('context: ', context);
+        console.log(self);
+        console.log(App);
+        var authDetails;
+
+        switch(provider) {
+          
+          case 'github':
+            authDetails = {
+              loginUrl: 'https://github.com/login/oauth/authorize?client_id=' + keys.github.clientID,
+              tokenUrl: 'https://github.com/login/oauth/access_token',
+              data: {
+                client_id : keys.github.clientID,
+                client_secret : keys.github.clientSecret
+              },
+              redirect_uri: 'https://bmlpebnpaikchpcabnbieodibjbhcggf.chromiumapp.org/githubToken'
             }
-          });
+            break;
+
+          case 'fitbit':
+            url = 'https://www.fitbit.com/oauth/request_token?oauth_consumer_key=' + keys.fitbit.consumerKey;
+            break;
+          case 'jawbone':
+            url = 'https://jawbone.com/auth/oauth2/auth?response_type=code&client_id=' + keys.jawbone.clientID;
+            break;
         }
-      );
-    },
+
+        chrome.identity.launchWebAuthFlow(
+          // This portion works with all APIs
+          {'url': authDetails.loginUrl, 'interactive': true},
+          function(redirectUrl) {
+            var code = redirectUrl.split('?code=')[1];
+            authDetails.data.code = code;
+
+            // This AJAX call is coded to work with github
+            // $.ajax({
+            //   type: 'POST',
+            //   url: authDetails.tokenUrl,
+            //   data: authDetails.data,
+            //   redirect_uri: authDetails.redirect_uri,
+            //   success: function(res) {
+            //     var token = res.match(/(?:access_token=)[a-zA-Z0-9]+/g)[0].split('access_token=')[1];
+            //     self.setState({userInfo: {github: {token: token }}});
+            //     // self.userInfo.github.token = token;
+            //     // self.userInfo.github.requestGithub('/user', function(res){
+            //     //   var github = self.userInfo.github;
+            //     //   github.username = ;
+            //     //   github.alias = ;
+            //     //   github.reposUrl = ;
+            //     // });
+            //   },
+            //   fail: function(err) {
+            //     console.error(err);
+            //   }
+            // });
+        
+          }
+        );
+      },
+
+    };
 
   },
 
@@ -81,6 +129,7 @@ var App = React.createClass({
     return (
       <div id="landscape-container">
         <Landscape user={this.state.userInfo} auth={this.auth} />
+        <span>{this.state}</span>
       </div>
     );
   }
