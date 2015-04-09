54a08010253a86c963f4d370498a630ec8605100
millionhari <mikemhari@gmail.com>
2015-04-09 12:28:32 -0700

    (cleanup) Delete test code for graphs

--- client/src/js/components/CommitsBox.jsx
+++ client/src/js/components/CommitsBox.jsx
@@ -3,6 +3,26 @@ var Chart = require('./Chart.jsx');
 
 var CommitsBox = React.createClass({
 
+  getInitialState: function(){
+    return {
+      commitsChart: this.generateChart()
+    }
+  },
+
+  generateChart: function(){
+    var chart = c3.generate({
+        bindto: '#chart',
+        data: {
+          columns: [
+            ['data1', 30, 200, 100, 400, 150, 250],
+            ['data2', 50, 20, 10, 40, 15, 25]
+          ]
+        }
+    });
+    console.log(chart);
+    return chart;
+  },
+
   render: function() {
     return (
       <div className="commits-box">
