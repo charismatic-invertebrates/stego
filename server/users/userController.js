cfac0b3820e09b28d60ed26c8fb348733b067356
David Rabkin <davidrabkin@gmail.com>
2015-04-10 15:48:37 -0700

    (refactor) Remove avatar backend functionality

--- server/users/userController.js
+++ server/users/userController.js
@@ -8,42 +8,14 @@ var User = require('./userModel.js');
 
 module.exports = {
 
-  // Retrieve type of user avatar
-  getAvatarType: function(req, res, next) {
-    var findUser = Q.nbind(User.findOne, User);
-
-    findUser({ githubID: req.body.githubID })
-      .then(function(user) {
-        res.json(user.avatar.type);
-      })
-      .fail(function(error) {
-        next(error);
-      });
-  },
-
-  // Retrieve name of user avatar
-  getAvatarName: function(req, res, next) {
-    var findUser = Q.nbind(User.findOne, User);
-
-    findUser({ githubID: req.body.githubID })
-      .then(function(user) {
-        res.json(user.avatar.name);
-      })
-      .fail(function(error) {
-        next(error);
-      });
-  },
-
   // Save a new user in our database
   saveUser: function(req, res, next) {
     var createUser = Q.nbind(User.create, User);
 
     var newUser = {
       githubID: req.body.githubID,
-      avatar: {
-        type: req.body.avatar.type,
-        name: req.body.avatar.name
-      }
+      fitbitID: req.body.fitbitID,
+      jawboneID: req.body.jawboneID,
     };
 
     createUser(newUser)
