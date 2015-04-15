c3a364311106eb93ec7fe89b2366f33731205d66
ChadFong <ChadMFong@Gmail.com>
2015-04-15 16:16:11 -0700

    (fix) lost a closing brace during rebase

--- server/auth/authRoutes.js
+++ server/auth/authRoutes.js
@@ -9,3 +9,5 @@ module.exports = function(app) {
   // Route from the /api/auth path. A POST request provides the code which we convert into a token by making a request from the relevant service.
   app.route('/getToken')
     .get(authController.getTokenFromCode);
+
+};
