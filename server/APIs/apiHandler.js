79abec36299b2ce17aefbc3ef1cfdfebc27f91d6
ChadFong <ChadMFong@Gmail.com>
2015-04-17 16:57:01 -0700

    (refactor) pull both github and fitness api calss to apiHandler

--- server/APIs/apiHandler.js
+++ server/APIs/apiHandler.js
@@ -29,11 +29,75 @@ module.exports = {
         userAccount.fitness.accessToken = JSON.parse(response[1]).access_token;
         return userAccount;
       })
-      .catch(function(error) {
-        console.error('Error in apiHandler.getTokens, token exchange failed');
+      .fail(function(error) {
+        console.error('Error in apiHandler.getTokens, token exchange failed: ', error);
       });
   },
 
+  getGithubData: function(userAccount) {
+    var githubUserParams = assignRequestParams('github', 'getUser', userAccount.github.accessToken);
 
+    return deferredRequest(githubUserParams)
+    // Store Github User information
+      .then(function(response) {
+        var githubUser = JSON.parse(response[1]);
+        userAccount.github.user = {
+          id: githubUser.id,
+          reposUrl: githubUser.repos_url,
+          commits: [],
+          username: githubUser.login,
+          name: githubUser.name
+        };
+      })
+      // Get Github Repo information
+      .then(function() {
+        var githubRepoParams = assignRequestParams('github', 'repos', userAccount.github);
+        return deferredRequest(githubRepoParams);
+      })
+      // Extract individual repo names and store:
+      .then(function(response){
+        var repos = JSON.parse(response[1]);
+        var repoList = [];
+        repos.forEach(function(repo) {
+          repoList.push(repo.name);
+        });
+        userAccount.github.repos = repoList;
+      })
+      // Extract commit information by repo and store on userAccount:
+      .then(function() {
+        var repoUrlsToCall = userAccount.github.repos.map(function(repo) {
+          return assignRequestParams('github', 'commits', userAccount, repo);
+        });
+
+        return Q.all(repoUrlsToCall.map(function(callParam) {
+          return deferredRequest(callParam);
+        }))
+          .then(function(results) {
+            results.forEach(function(response, index) {
+              userAccount.github.user.commits.push({
+                repo: userAccount.github.repos[index],
+                commitsByRepo: JSON.parse(response[1])
+              });
+            });
+          });
+      })
+      .fail(function(error) {
+        console.error('Error in apiHandler.getGithubData, failed to get Github data: ', error);
+      });
+  },
+
+  getFitnessData: function(userAccount) {
+    var fitnessStepsParams = assignRequestParams(userAccount.fitness.provider, 'steps', userAccount.fitness.accessToken);
+
+    return deferredRequest(fitnessStepsParams)
+      // Store user's steps
+      .then(function(response) {
+        userAccount.fitness.user = JSON.parse(response[1]).data;
+        return userAccount;
+      })
+      .fail(function(error) {
+        console.error('Error in apiHandler.getFitnessData, failed to get steps: ', error);
+      });
+  }
 
 };
