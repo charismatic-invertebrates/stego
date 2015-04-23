b49abf4c95bc0f8afb73f3fa85267bd8689e92d7
ChadFong <ChadMFong@Gmail.com>
2015-04-23 15:58:21 -0700

    (add) put buttons back on page

--- client/src/js/components/StepsBox.jsx
+++ client/src/js/components/StepsBox.jsx
@@ -20,10 +20,6 @@ var StepsBox = React.createClass({
   },
   
   render: function() {
-        // <a className="button" onClick={this.getProviderCode.bind(null, 'fitbit')}>Login to FitBit</a>
-        // <a className="button" onClick={this.getProviderCode.bind(null, 'jawbone')}>Login to Jawbone</a>
-        // <a className="button" onClick={this.logUser}>Console log user info</a>
-        // <a className="button" onClick={this.getProviderCode.bind(null, 'github', true)}>Login to Pre-existing Account</a>
     return (
       <div className="steps-box">
         <h2>Steps</h2>
